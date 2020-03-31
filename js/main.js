1d199c8a4ad97710479a6595b7be034719667d89
Luiz Rodrigues <luizrodriguez42@gmail.com>
2020-03-31 15:55:19 -0300

    Feito no main.js a função para mostrar os dados do veiculo cadastrado, e no index.html feito tabela para exibir as informações.

--- js/main.js
+++ js/main.js
@@ -17,10 +17,28 @@ function cadastrarVeiculo(e){
         carros.push(carro)
         localStorage.setItem('patio', JSON.stringify(carros))
     }else{
-        var carros = JSON.parse(localStorage.getItem('estacionamento'))
+        var carros = JSON.parse(localStorage.getItem('patio'))
         carros.push(carro)
         localStorage.setItem('patio', JSON.stringify(carros))
     }
 
+    mostraPatio()
+
     e.preventDefault()
+}
+
+function mostraPatio(){
+    var carros = JSON.parse(localStorage.getItem('patio'))
+    var carrosResultado = document.getElementById('resultados')
+
+    carrosResultado.innerHTML = ''
+
+    for( var i = 0; i < carros.length; i++){
+        var modelo = carros[i].modelo
+        var placa = carros[i].placa
+        var hora = carros[i].hora
+        var minutos = carros[i].minutos
+
+        carrosResultado.innerHTML += '<tr><td>' + modelo + '</td><td>' + placa + '</td><td>' + hora + ' : ' + minutos + '</td></tr>'
+    }
 }
